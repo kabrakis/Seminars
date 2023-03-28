d32e15580ee314184541eb0e1c9fe72bee8dbada
Ivan <zag091@mail.ru>
2023-03-28 23:20:16 +0300

    9

---
+++ sem2task14/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №14
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine(" Введите число");
+int num = int.Parse(Console.ReadLine()??"o"); 
+bool test1 = (num%7==0);
+bool test2 = (num%23==0);
+if(test1&&test2)
+{
+Console.WriteLine("число делиться на 23 и на 7");
+}
+else
+{
+Console.WriteLine("число не делиться на 23 и на 7");
+}
