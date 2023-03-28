6cf238f391269b1773c30fb1fe7186a07e7b7c8b
Ivan <zag091@mail.ru>
2023-03-28 23:15:59 +0300

    first dz

---
+++ sem1DZtask2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число");
+int numberOne = int.Parse(Console.ReadLine()??"o");
+Console.WriteLine("Введите второе число");
+int numberTwo = int.Parse(Console.ReadLine()??"o");
+  if(numberOne>numberTwo)
+ {
+    Console.WriteLine("максимальное число это "+numberOne);
+ }
+ else
+ {
+    Console.WriteLine("максимальное число это "+numberTwo);
+ }
