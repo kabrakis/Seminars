86c34691e60479713fba4855652665c5e7d1ce98
Ivan <zag091@mail.ru>
2023-03-28 23:18:55 +0300

    5 task

---
+++ seminar1task05/Program.cs
@@ -0,0 +1,13 @@
+﻿string? numLine = Console.ReadLine();
+if (numLine != null)
+{
+    int numN = int.Parse(numLine);
+    int startNum = numN * (-1);
+    string res = String.Empty;
+    while (startNum <= numN)
+    {
+        startNum++;
+        res = res + startNum + " ";
+    }
+    Console.WriteLine(res);
+}
