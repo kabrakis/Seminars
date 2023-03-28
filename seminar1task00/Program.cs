68eaa57d5cf002a60573c6ec3e80a1a4560f5740
Ivan <zag091@mail.ru>
2023-03-28 23:17:49 +0300

    First task

---
+++ seminar1task00/Program.cs
@@ -0,0 +1,17 @@
+﻿// Считываем данные с консоли
+string? inline = Console.ReadLine();
+//Проверяем, чтобы данные были не пустыми
+if(inline!=null)
+{
+
+     //Парсим введеное число
+    int inNuber = int.Parse(inline);
+    int outStrPow = (int)Math.Pow(inNuber,2);
+
+    //int outNumberTest = inline.ToCharArray()[0]*inline.ToCharArray()[0];
+    // находим квадрат числа
+    int outNumber = inNuber*inNuber;
+
+    //Выводим данные в консоль
+    Console.WriteLine(outStrPow);
+}
