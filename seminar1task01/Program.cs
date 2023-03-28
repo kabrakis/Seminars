11ad12e630a5f4b3302f9f5f8922b94330db0cbe
Ivan <zag091@mail.ru>
2023-03-28 23:17:57 +0300

    2 task

---
+++ seminar1task01/Program.cs
@@ -0,0 +1,15 @@
+﻿string? numLine1 = Console.ReadLine();
+string? numLine2 = Console.ReadLine();
+if(numLine1!=null && numLine2 != null)
+{
+    int num1 = int.Parse(numLine1);
+    int num2 = int.Parse(numLine2);
+    if (num1 == num2*num2)
+    {
+        Console.WriteLine("Первое число является квадратом второго");
+    }
+    else
+    {
+        Console.WriteLine("Нет, не является");
+    }
+}
