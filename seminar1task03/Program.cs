ea9433f0ceaa871795d54b171f3aa01f3ff6cede
Ivan <zag091@mail.ru>
2023-03-28 23:18:06 +0300

    3 task

---
+++ seminar1task03/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите день недели");
+string? dayLine = Console.ReadLine();
+if (dayLine!=null)
+{
+    string [] dayWeek = new string [7];
+    dayWeek [0] = "понедельник";
+    dayWeek [1] = "вторник";
+    dayWeek [2] = "среда";
+    dayWeek [3] = "четверг";
+    dayWeek [4] = "пятница";
+    dayWeek [5] = "суббота";
+    dayWeek [6] = "восскресенье";
+    int day = int.Parse(dayLine);
+    Console.WriteLine(dayWeek[day-1]);
+}
