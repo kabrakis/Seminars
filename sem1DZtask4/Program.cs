3b8cd4b07fe3731d3be46e6743ce901923271823
Ivan <zag091@mail.ru>
2023-03-28 23:16:12 +0300

    Second dz

---
+++ sem1DZtask4/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите количество сравниваемых чисел");
+            int index, lengthArray = int.Parse(Console.ReadLine()??"o");
+            int[] array = new int[lengthArray];
+            Console.WriteLine("Введите эти числа через пробел:");
+            string[] str = Console.ReadLine().Split(new char[] { ' ', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            for (index = 0; index < (lengthArray < str.Length ? lengthArray : str.Length); ++index)
+                array[index] = Convert.ToInt32(str[index]);
+            Console.WriteLine("Введенные числа (после прочтения нажмите enter)");
+            for (index = 0; index < lengthArray; ++index)
+                Console.Write("{0} ", array[index]);
+            Console.ReadLine();
+int max = array[0];
+int count = lengthArray;
+index = 0;
+while (count>index)
+if(max>array[index])
+{
+    index++;
+}
+else
+{
+    max = array[index];
+    index++;
+}
+Console.WriteLine("Максимальное число из введеных "+max);
