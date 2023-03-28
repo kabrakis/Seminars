475ef2233a7f65f4322c15ba2cc792a1f2aa3c8d
Ivan <zag091@mail.ru>
2023-03-28 23:20:30 +0300

    10

---
+++ sem2task16/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №16
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+Console.WriteLine(" Введите 1-e число");
+int num1 = int.Parse(Console.ReadLine()??"o"); 
+Console.WriteLine(" Введите 2-e число");
+int num2= int.Parse(Console.ReadLine()??"o"); 
+void SqrtTest (int a, int b) 
+{
+if(a == b*b)
+{
+Console.WriteLine(a+" квадрат "+b);
+}
+else
+{
+Console.WriteLine(a+" не квадрат "+b);    
+}
+}
+SqrtTest(num1,num2);
+SqrtTest(num2,num1);
