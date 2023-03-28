b175c7ebd5c61b3ddb936354492724ce66b75c34
Ivan <zag091@mail.ru>
2023-03-28 23:18:23 +0300

    4 task

---
+++ seminar1task07/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число");
+string? numLine = Console.ReadLine();
+if (numLine != null)
+{
+//    char[] charArray = numLine.ToCharArray();
+//    Console.WriteLine(charArray[charArray.Length-1]);
+int num = int.Parse(numLine);
+int res = num % 10;
+Console.WriteLine(res);
+}
+
