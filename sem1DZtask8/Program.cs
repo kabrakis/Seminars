c442d323ad59f57c0302c803fb44a0ee1412c223
Ivan Rizhov <97846647+kabrakis@users.noreply.github.com>
2023-03-29 16:53:50 +0300

    Update Program.cs

--- sem1DZtask8/Program.cs
+++ sem1DZtask8/Program.cs
@@ -1,8 +1,6 @@
 ﻿// Напишите программу, которая на вход принимает число (N),
 //  а на выходе показывает все чётные числа от 1 до N.
-Console.WriteLine("Введите число до которого будет выводиться значения");
 int number= int.Parse(Console.ReadLine()??"o");
-int count = 0;
 int outNumber = 1;
 while (outNumber <= number)
 {
