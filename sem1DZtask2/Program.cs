a3c50f51603796a21a7a57dfb9ffdb3f793e13b7
Ivan Rizhov <97846647+kabrakis@users.noreply.github.com>
2023-03-29 08:37:06 +0300

    Update Program.cs

--- sem1DZtask2/Program.cs
+++ sem1DZtask2/Program.cs
@@ -4,11 +4,13 @@ Console.WriteLine("Введите первое число");
 int numberOne = int.Parse(Console.ReadLine()??"o");
 Console.WriteLine("Введите второе число");
 int numberTwo = int.Parse(Console.ReadLine()??"o");
-  if(numberOne>numberTwo)
+int max = 0;
+int min = 0;
+ if(numberOne>numberTwo)
  {
-    Console.WriteLine("максимальное число это "+numberOne);
+    Console.WriteLine("максимальное число это "+numberOne, "минимальное это "+numberTwo);
  }
  else
  {
-    Console.WriteLine("минимальное число это "+numberTwo);
+    Console.WriteLine("минимальное число это "+numberOne, "максимальное число это "+numberTwo);
  }
