390a5e67c6af72fea80e5f41ccb45f4a51f2d040
Ivan <zag091@mail.ru>
2023-04-02 11:55:32 +0300

    finish

---
+++ sem3task22/Program.cs
@@ -0,0 +1,26 @@
+﻿//   Задача №22
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+  
+  
+  
+    int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int res =  int.Parse(Console.ReadLine() ?? ("0"));
+
+    return res;
+}
+
+string lineGenerator (int num, int pow)
+{
+    string res = String.Empty;
+    for(int i =1; i<=num; i++)
+    {
+        res+=Math.Pow(i,pow)+" ";
+    }
+    return res;
+}
+int numN = ReadData("Введите N");
+
+Console.WriteLine(lineGenerator(numN,1));
+Console.WriteLine(lineGenerator(numN,2));
