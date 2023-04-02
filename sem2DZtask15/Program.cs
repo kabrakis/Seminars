83ff9427e436db3fd83cfe38b1e1ec65270bdcd4
Ivan <zag091@mail.ru>
2023-04-02 11:54:09 +0300

    finish

---
+++ sem2DZtask15/Program.cs
@@ -0,0 +1,16 @@
+﻿int day = int.Parse(Console.ReadLine() ?? "o"); 
+if(day<=0 || day >7) // меньше или равно нули ИЛИ больше 7
+{
+    Console.WriteLine("Это не день недели");
+}
+else
+{
+    if(day>=1 && day <= 5)
+    {
+        Console.WriteLine("Будний день");
+    }
+    else
+    {
+        Console.WriteLine("Выходной день");
+    }
+}
