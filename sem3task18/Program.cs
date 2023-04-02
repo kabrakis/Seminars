4c84a12015bdd79e068c18fa57473fd31e2998de
Ivan <zag091@mail.ru>
2023-04-02 11:55:00 +0300

    finish

---
+++ sem3task18/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача №18
+// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+    int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int res =  int.Parse(Console.ReadLine() ?? ("0"));
+
+    return res;
+}
+
+void PrintAnwser (int num)
+{
+    if (num == 1 ) Console.WriteLine("x>0,y>0");
+    if (num == 2 ) Console.WriteLine("x>0,y<0");
+    if (num == 3 ) Console.WriteLine("x<0,y<0");
+    if (num == 4 ) Console.WriteLine("x<0,y>0");
+} 
+
+int num = ReadData ("Введите четверть");
+if(num>0&&num<5)
+{
+PrintAnwser(num);
+}
+else
+{
+    Console.WriteLine("Попробуй еще раз, четвертей то всего 4");
+}
