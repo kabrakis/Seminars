75944e7b587211dc9de62a106fed61e2a6ed2655
Ivan <zag091@mail.ru>
2023-04-04 21:03:39 +0300

    Finish

---
+++ sem3DZtask19/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+bool Paltest(int num)
+{
+    bool res = false;
+    int d1 = num / 10000;
+    int d5 = num % 10;
+    int d2 = (num / 1000) % 10;
+    int d4 = (num / 10) % 10;
+    if (d1 == d5 && d2 == d4)
+        res = true;
+    return res;
+}
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int res = int.Parse(Console.ReadLine() ?? ("0"));
+
+    return res;
+}
+
+int num = ReadData("Введите число: ");
+Console.WriteLine( Paltest(num));
