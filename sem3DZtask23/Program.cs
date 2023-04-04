8b2c7a12b64a9c7f42469f67f515ba41aec43fab
Ivan <zag091@mail.ru>
2023-04-04 21:17:40 +0300

    Finish+comm

--- sem3DZtask23/Program.cs
+++ sem3DZtask23/Program.cs
@@ -1,23 +1,25 @@
 ﻿//  Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.   
-    
-    int ReadData(string msg)
+
+//функиця ввода данных
+int ReadData(string msg)
 {
     Console.WriteLine(msg);
-    int res =  int.Parse(Console.ReadLine() ?? ("0"));
+    int res = int.Parse(Console.ReadLine() ?? ("0"));
 
     return res;
 }
 
-string lineGenerator (int num, int pow)
+//функция возведения числа в степень до числа N
+string LineGenerator(int num, int pow)
 {
     string res = String.Empty;
-    for(int i =1; i<=num; i++)
+    for (int i = 1; i <= num; i++)
     {
-        res+=Math.Pow(i,pow)+" ";
+        res += Math.Pow(i, pow) + " ";
     }
     return res;
 }
 int numN = ReadData("Введите N");
 
-Console.WriteLine(lineGenerator(numN,1));
-Console.WriteLine(lineGenerator(numN,3));
+Console.WriteLine(LineGenerator(numN, 1));
+Console.WriteLine(LineGenerator(numN, 3));
