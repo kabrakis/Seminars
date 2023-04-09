a6124c6c8d8b264ab3b4219ba3297427f5947a9c
Ivan <zag091@mail.ru>
2023-04-09 08:47:24 +0300

    finish

---
+++ sem4DZtask27/Program.cs
@@ -0,0 +1,31 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// метод чтени данных
+    int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int res =  int.Parse(Console.ReadLine() ?? ("0"));
+
+    return res;
+}
+//метод вывода результата
+void PrintResult(string msg, int res)
+{
+    Console.WriteLine(msg+res);
+}
+
+//  метод подсчитывания суммы всех цифр в числе
+int SumAllNum(int num)
+{
+    int res = 0;
+    while(num>0)
+    {
+        res+=num%10;
+        num=num/10;
+        
+    }
+    return res;
+}
+int num = ReadData("Введите число: ");
+int res =SumAllNum(num);
+PrintResult("Сумма всех цифр в числе: ",res);
