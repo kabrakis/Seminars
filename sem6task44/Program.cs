54233791d914360b1a256029fce5a3ae3ebaf67c
Ivan <zag091@mail.ru>
2023-04-12 21:54:35 +0300

    Finish

---
+++ sem6task44/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача №44
+// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+//  метод считывания данных
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int res = int.Parse(Console.ReadLine() ?? ("0"));
+
+    return res;
+}
+
+//Метод построения чисел фибаначи
+string FibNum(int num)
+{
+    string res = "0 1";
+    long first = 0;
+    long last = 1;
+    for (int i = 2; i <= num; i++)
+    {
+        res += " " + (first + last);
+        (first, last) = (last, last + first);
+    }
+    return res;
+}
+
+string res = "";
+int n = ReadData("Введите n");
+res = FibNum(n);
+Console.WriteLine(res);
