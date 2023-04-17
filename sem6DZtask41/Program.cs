49089895ddafe16e6c949f6c71c3507c004db0e5
Ivan <zag091@mail.ru>
2023-04-17 19:16:31 +0300

    Finish

---
+++ sem6DZtask41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+
+// метод считывания данных
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int res = int.Parse(Console.ReadLine() ?? ("0"));
+
+    return res;
+}
+
+// метод для проверки числа, больше-ли оно нуля
+int CountPosNum(int num)
+{
+    int count = 0;
+
+    while (num > 0)
+    {
+        int val = ReadData("Введите число: ");
+
+        if (val > 0)
+        {
+            count++;
+        }
+        num--;
+
+    }
+    return count;
+}
+
+
+
+int num = ReadData("Сколько чисел будем сравнивать? ");
+
+// проверим num на ноль или отрицательное значние
+if (num <= 0)
+{
+    Console.WriteLine("Колличество сравниваемых числе не должно быть меньше или равно нулю!");
+}
+else
+{
+    int res = CountPosNum(num);
+    Console.WriteLine("Чисел больше нуля: " + res);
+}
