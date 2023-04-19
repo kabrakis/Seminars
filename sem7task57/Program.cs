f26ffc0f68d9cd7834318a97700940375e796cbb
Ivan <zag091@mail.ru>
2023-04-19 22:27:07 +0300

    Finish

---
+++ sem7task57/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задача №57
+// Составить частотный словарь элементов
+// двумерного массива. Частотный словарь содержит
+// информацию о том, сколько раз встречается
+// элемент входных данных.
+
+// метод считывания данных
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int res = int.Parse(Console.ReadLine() ?? ("0"));
+
+    return res;
+}
+
+void Print1DArray(int[] arr)
+
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ",");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+//метод генерации массива
+int[,] Gen2Darray(int countRow, int countColum, int minValue, int maxValue)
+
+{
+    int[,] arr = new int[countRow, countColum];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColum; j++)
+        {
+            arr[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return arr;
+}
+
+
+//метод печати двумерного массива
+void Print2DArray(int[,] arr)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[new Random().Next(0, 16)];
+            Console.Write(arr[i, j] + " ");
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+
+//Метод подсчета (упрощенный)
+int[] FreqArr(int[,] arr, int count)
+{
+    int[] outArr = new int[count];
+    for (int i = 0; i < arr.GetLength(0); i++) //GetLength - до конца строки(0)/столбца(1)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            outArr[arr[i,j]]++;
+        }
+    }
+    return outArr;
+}
+
+int row = ReadData("Введите количество строк: ");
+int col = ReadData("Введите количество столбцов: ");
+int[,] arr2D = Gen2Darray(row, col, 0, 9);
+Print2DArray(arr2D);
+Console.WriteLine();
+int[] outMatr = FreqArr(arr2D,10);
+Print1DArray(outMatr);
