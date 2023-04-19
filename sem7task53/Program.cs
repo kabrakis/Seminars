626fbeb496951cd26dee834b2c89309983550ab3
Ivan <zag091@mail.ru>
2023-04-19 22:26:47 +0300

    Finish

---
+++ sem7task53/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача №53
+// Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю
+// строку массива.
+
+// метод считывания данных
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int res = int.Parse(Console.ReadLine() ?? ("0"));
+
+    return res;
+}
+
+//метод генерации массива
+int[,] Gen2Darray(int countRow, int countColum, int minValue, int maxValue)
+
+{
+    int[,] arr = new int[countRow, countColum];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColum; j++)
+        {
+            arr[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return arr;
+}
+
+
+//метод печати двумерного массива
+void Print2DArray(int[,] arr)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[new Random().Next(0, 16)];
+            Console.Write(arr[i, j] + " ");
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void  Change2DArray (int[,] matr)
+{
+int temp = 0;
+for (int i=0; i<matr.GetLength(1);i++)
+{
+    temp = matr [0,i];
+    matr[0,i]=matr[matr.GetLength(1)-1,i];
+    matr[matr.GetLength(1)-1,i] = temp;
+}
+}
+
+int row = ReadData("Введите количество строк: ");
+int col = ReadData("Введите количество столбцов: ");
+int[,] arr2D = Gen2Darray(row, col, 10, 99);
+Print2DArray(arr2D);
+Console.WriteLine();
+Change2DArray(arr2D);
+Print2DArray(arr2D);
