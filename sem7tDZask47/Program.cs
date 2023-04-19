03c26e41a778d354ffdce40290cbe20748137542
Ivan <zag091@mail.ru>
2023-04-19 22:27:36 +0300

    Finish

---
+++ sem7tDZask47/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// метод считывания данных
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int res = int.Parse(Console.ReadLine() ?? ("0"));
+
+    return res;
+}
+
+//метод генерации массива с вещественными числами
+double[,] Gen2DarrayReal(int countRow, int countColum, int minValue, int maxValue)
+
+{
+    double[,] arr = new double[countRow, countColum];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColum; j++)
+        {
+            arr[i, j] = new Random().Next(minValue, maxValue + 1) + new Random().NextDouble();
+        }
+    }
+    return arr;
+}
+
+//метод печати двумерного массива с вещественными числами
+void Print2DArrayReal(double[,] arr)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[new Random().Next(0, 16)];
+            Console.Write(arr[i, j] + " ");
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = ReadData("Введите количество строк: ");
+int col = ReadData("Введите количество столбцов: ");
+double[,] arr2D = Gen2DarrayReal(row, col, 0, 99);
+Print2DArrayReal(arr2D);
+
