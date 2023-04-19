9b09a69b037f83cf8b9aa71ba0b724d043755233
Ivan <zag091@mail.ru>
2023-04-19 22:26:59 +0300

    Finish

---
+++ sem7task55/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача №55
+// Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае,
+// если это невозможно, программа должна вывести
+// сообщение для пользователя.
+
+// метод считывания данных
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int res = int.Parse(Console.ReadLine() ?? ("0"));
+
+    return res;
+}
+
+//метод генерации массива
+int[,] Gen2Darray(int countRow, int countColum, int minValue, int maxValue)
+
+{
+    int[,] arr = new int[countRow, countColum];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColum; j++)
+        {
+            arr[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return arr;
+}
+
+
+//метод печати двумерного массива
+void Print2DArray(int[,] arr)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[new Random().Next(0, 16)];
+            Console.Write(arr[i, j] + " ");
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+
+//Метод замены строк на столбцы
+int[,] Transp(int[,] Matr)
+{
+    int[,] outMatr = new int[Matr.GetLength(0),Matr.GetLength(1)];
+    for (int i = 0; i < Matr.GetLength(0); i++) //GetLength - до конца строки(0)/столбца(1)
+    {
+        for (int j = 0; j < Matr.GetLength(1); j++)
+        {
+            outMatr[i,j] = Matr[j,i];
+        }
+    }
+    return outMatr;
+}
+
+int row = ReadData("Введите количество строк: ");
+int col = ReadData("Введите количество столбцов: ");
+int[,] arr2D = Gen2Darray(row, col, 10, 99);
+Print2DArray(arr2D);
+Console.WriteLine();
+int[,] outMatr = Transp(arr2D);
+Print2DArray(outMatr);
