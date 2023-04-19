0cdbfee277a89da090e86b29a5ee6337e1338fb3
Ivan <zag091@mail.ru>
2023-04-19 22:27:20 +0300

    Finish

---
+++ sem7task61/Program.cs
@@ -0,0 +1,40 @@
+﻿//треугольник Паскаля
+
+// метод считывания данных
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int res = int.Parse(Console.ReadLine() ?? ("0"));
+
+    return res;
+}
+
+long Factor(int n)
+{
+    long res = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        res *= i;
+    }
+    return res;
+}
+
+void PrintPascalTriangle(int nRaw)
+{
+    for (int i = 0; i < nRaw; i++)
+    {
+        for(int k =0; k<nRaw-i;k++)
+        {
+            Console.Write(" ");
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            Console.Write(" ");
+            Console.Write(Factor(i) / (Factor(j) * Factor(i - j)));
+        }
+        Console.WriteLine();
+    }
+}
+
+int countRaw = ReadData("Введите колличество строк треугольника Паскаля");
+PrintPascalTriangle(countRaw);
