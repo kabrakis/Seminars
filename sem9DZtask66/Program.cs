86fbce38320296dd42372488a6292ece59e14ce9
Ivan <zag091@mail.ru>
2023-04-23 11:21:54 +0300

    Finish

---
+++ sem9DZtask66/Program.cs
@@ -0,0 +1,13 @@
+﻿int GetSumBetweenNandM(int m, int n)
+{
+    if (n > m)
+        return n + GetSumBetweenNandM(m, n - 1);
+    return m;
+}
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = GetSumBetweenNandM(m, n);
+Console.WriteLine($"Сумма чисел от {m} до {n} равна {result}"); 
+
